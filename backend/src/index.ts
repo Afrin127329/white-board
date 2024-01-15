da7a3311408707fa308353d9850c0bbe4376df16
Afrin Nahar <afrinnahar1999@gmail.com>
2024-01-15 19:36:32 +0600

    Added socket.io on server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import { createServer } from "node:http";
+import { Server, Socket } from "socket.io";
 
 const app = express();
 const server = createServer(app);
-
+const io = new Server(server);
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+io.on("connection", (socket: Socket) => {
+  console.log("User connected!");
 });
 
 server.listen(8000, () => {
